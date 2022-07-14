677978b958e31ecb53ecdc2488323afb66ab1bd2
Vyacheslav <ext_vboriskin@rencredit.ru>
2022-07-14 20:05:07 +0300

    spring boot starter consumer

--- Unit13_SpringStarterConsumer/src/main/java/com/company/CurrencyConfig.java
+++ Unit13_SpringStarterConsumer/src/main/java/com/company/CurrencyConfig.java
@@ -1,2 +1,41 @@
-package com.company;public class CurrencyConfig {
+package com.company;
+
+import com.company.currency.Currency;
+import com.company.currency.EurCurrency;
+import com.company.currency.RubCurrency;
+import com.company.currency.UsdCurrency;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class CurrencyConfig {
+    @Bean
+    public Currency rubbleCurrency() {
+        return new RubCurrency() {
+            @Override
+            public String printName() {
+                return "NEW RUB";
+            }
+        };
+    }
+
+    @Bean
+    public Currency usdCurrency() {
+        return new UsdCurrency() {
+            @Override
+            public String printName() {
+                return "NEW USD";
+            }
+        };
+    }
+
+    @Bean
+    public Currency eurCurrency() {
+        return new EurCurrency() {
+            @Override
+            public String printName() {
+                return "NEW EUR";
+            }
+        };
+    }
 }
--- Unit13_SpringStarterConsumer/src/main/java/com/company/CurrencyService.java
+++ Unit13_SpringStarterConsumer/src/main/java/com/company/CurrencyService.java
@@ -1,2 +1,24 @@
-package com.company;public class CurrencyService {
+package com.company;
+
+import com.company.currency.Currency;
+import com.company.currency.EurCurrency;
+import com.company.currency.RubCurrency;
+import com.company.currency.UsdCurrency;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.stream.Stream;
+
+@Service
+@RequiredArgsConstructor
+public class CurrencyService {
+    private final Currency currency;
+
+    @PostConstruct
+    public void init() {
+        System.out.printf("Валюта %s, курс %s ",
+                currency.printName(),
+                currency.courseByRubble().toString());
+    }
 }
